97a06a4575f2d5c7434bc29e555a92ae2dbb650c
ExoPlant <lennyj2468+github@gmail.com>
2021-08-21 21:36:00 +0100

    Rename stuff

--- src/main/java/com/frogastudios/mopalettes/MoPalettes.java
+++ src/main/java/com/frogastudios/mopalettes/MoPalettes.java
@@ -1,8 +1,8 @@
-package com.frogastudios.mopallets;
+package com.frogastudios.mopalettes;
 
 import net.fabricmc.api.ModInitializer;
 
-public class Mopallets implements ModInitializer {
+public class MoPalettes implements ModInitializer {
     @Override
     public void onInitialize() {
 
--- src/main/java/com/frogastudios/mopalettes/client/MoPalettesClient.java
+++ src/main/java/com/frogastudios/mopalettes/client/MoPalettesClient.java
@@ -1,11 +1,11 @@
-package com.frogastudios.mopallets.client;
+package com.frogastudios.mopalettes.client;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
 @Environment(EnvType.CLIENT)
-public class MopalletsClient implements ClientModInitializer {
+public class MoPalettesClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
 
