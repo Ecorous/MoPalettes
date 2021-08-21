94b0ca5ed9822fb41b42139e50639c6d8561e343
ExoPlant <lennyj2468+github@gmail.com>
2021-08-21 21:24:20 +0100

    Gradle stuff

---
+++ src/main/java/com/frogastudios/mopallets/Mopallets.java
@@ -0,0 +1,10 @@
+package com.frogastudios.mopallets;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Mopallets implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/frogastudios/mopallets/client/MopalletsClient.java
@@ -0,0 +1,13 @@
+package com.frogastudios.mopallets.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class MopalletsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
